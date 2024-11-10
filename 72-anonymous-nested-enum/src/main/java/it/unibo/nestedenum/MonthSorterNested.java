64aa1c12c6b54106df796061106d39b7af8806e3
PietroSiboni <pietro.siboni3@studio.unibo.it>
2024-11-10 10:24:06 +0100

    modified second exercise(uncompleted)

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -18,4 +18,35 @@ public final class MonthSorterNested implements MonthSorter {
     public Comparator<String> sortByOrder() {
         return null;
     }
+
+    private enum Month{
+
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private final int days;
+
+        Month(final int days){
+            this.days=days;
+        }
+    }  
+
+    public static Month fromString(String name){
+        Objects.requireNonNull(name);
+        try {
+            return valueOf(name);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 }
